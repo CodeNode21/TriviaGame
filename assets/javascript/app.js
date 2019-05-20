9fd734f13c79ae10c067c21a26129586ac1e3c9a
CodeNode21 <mbednara@gmail.com>
2019-05-20 18:32:34 -0400

    Added Questions, answers and coices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,43 @@
 var correct = 0;
 var incorrect = 0;
 
+var trivia = {
+questions: {
+	q1: "Which US state is named on the label of a Jack Daniels bottle?",
+	q2: "Which state is known as the Peach state?"
+	q3: "What is the smallest U.S. state?"
+	q4: "What is the tallest mountain in the United States?"
+	q5: "Which of the following states does NOT border the Great Lakes?"
+},
+choices: {
+	q1: ["New York", "Alabama", "Tennesse", "Arkansas"],
+	q2: ["Florida", "Georgia", "California", "Mississippi"],
+	q3: ["Connecticut", "Delaware", "Vermont", "Rhode Island"],
+	q4: ["Mount McKinley", "Mount Hood", "Mount Ranier", "Mount Rushmore"],
+	q5: ["Ohio", "Michigan", "Iowa", "Illinois"],
+},
+answers: {
+	q1: "Tennesse",
+	q2: "Georgia",
+	q3: "Rhode Island",
+	q4: "Mount McKinley",
+	q5: "Illinois"
+}
+},
 
-function startGame()
+function startGame(){
+	
 
 function timer()
 
 function resetGame()
+
+
+// press start (
+// timer starts running
+// question 1 and choices diplayed
+// listen for correct choice or timer running out
+// register result, move on to next question
+// question 2 and choices displayed
+// listen for correct choice or timer running out
+// register result, move on to next question
