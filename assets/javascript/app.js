ceb67824d37e71601aba967f7bf327a81d0a10ac
CodeNode21 <mbednara@gmail.com>
2019-05-20 13:11:53 -0400

    js pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//press start to start the trivia
+//user answers questions before timer runs down
+//user can only choose one answer
+//at end of game correct and incorrect answers are displayed
+//start over option available
+//questions have 4 choices, 1 correct answer
+
+var correct = 0;
+var incorrect = 0;
+
+
+function startGame()
+
+function timer()
+
+function resetGame()
